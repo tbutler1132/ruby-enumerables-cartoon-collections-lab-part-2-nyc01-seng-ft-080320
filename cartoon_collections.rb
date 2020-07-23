1361243b5ce1f6f87e49c7c5931ff093556bc3d3
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 15:11:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element|
+  planeteer_calls.each do |element|
     element.capitalize
+  end 
+  planeteer_calls.map do |element|
     element << "!"
   end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
